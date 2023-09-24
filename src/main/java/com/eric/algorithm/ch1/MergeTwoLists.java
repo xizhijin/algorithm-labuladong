5c88aa512e4ce577d4796e3cbe570ee70a678851
eric <xizhijin@yeah.net>
2023-09-24 23:04:01 +0800

    Merge two list.

---
+++ src/main/java/com/eric/algorithm/ch1/MergeTwoLists.java
@@ -0,0 +1,48 @@
+package com.eric.algorithm.ch1;
+
+public class MergeTwoLists {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1), p = dummy;
+        ListNode p1 = l1, p2 = l2;
+        while (p1 != null && p2 != null) {
+            if (p1.val > p2.val) {
+                p.next = p2;
+                p2 = p2.next;
+            } else {
+                p.next = p1;
+                p1 = p1.next;
+            }
+            p = p.next;
+        }
+
+        if (p1 != null) {
+            p.next = p1;
+        }
+
+        if (p2 != null) {
+            p.next = p2;
+        }
+
+        return dummy.next;
+    }
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode() {
+
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
